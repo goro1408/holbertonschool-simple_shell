cb36c0f73427debad4f6adea5c236af47897048d
Gabriel <gabyrivm06@gmail.com>
2024-08-20 14:41:48 -0400

    header file

---
+++ shell.h
@@ -0,0 +1,21 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/* ======= LIBRARIES ======= */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/* ======= GLOBAL VARIABLES ======= */
+
+extern char **environ;
+
+/* ======= PROTOTYPES ======= */
+
+void prompt(void);
+
+#endif
