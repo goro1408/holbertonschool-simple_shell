fddf1efedfa8437199711792cc74014dc10c170e
Gabriel <gabyrivm06@gmail.com>
2024-08-21 10:25:22 -0400

    added a function to prompt.c

--- prompt.c
+++ prompt.c
@@ -9,3 +9,11 @@ void prompt(void)
 	printf("$ ");
 	fflush(stdout);
 }
+
+size_t strcspn(const char s, const charreject) {
+    const char p = s;
+    while (p && !strchr(reject, *p)) {
+        p++;
+    }
+    return p - s;
+}
