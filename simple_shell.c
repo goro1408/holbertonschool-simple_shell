de57ade13cec3a3da657d64b9a847ac64fc1aeed
Gabriel <gabyrivm06@gmail.com>
2024-08-21 19:02:06 -0400

    change the code in the main function to use the new functions.

--- simple_shell.c
+++ simple_shell.c
@@ -1,77 +1,31 @@
 #include "shell.h"
 
 /**
- * main - Simple-Shell.
- * @ac: argument count
- * @argv: argument vector
+ * main - Entry point for the shell program.
+ * @argc: Argument count.
+ * @argv: Argument vector.
  *
- * Return: 0
  */
-
-int main(int ac, char **argv)
+int main(int argc, char **argv)
 {
-	char *command = NULL, *av[2], *token;
-	int status, i = 0;
-	size_t n = 0;
-	ssize_t nread;
-	pid_t pid;
-	(void)ac;
+	int status = 1;
+	int is_interactive = isatty(STDIN_FILENO);
+	char *command = NULL;
+	char **array;
+	(void)argc;
+	(void)argv;
 
-	while (1)
+
+	while (status)
 	{
-
-		if (isatty(STDIN_FILENO))
+		if (is_interactive)
 		{
 			prompt();
 		}
-
-		nread = getline(&command, &n, stdin);
-		command[_strcspn(command, "\n")] = '\0';
-
-		if (nread == -1)
-		{
-			perror("Exiting shell");
-			free(command);
-			exit(0);
-		}
-		
-		if (strcmp(command, "exit") == 0)
-		{
-			free(command);
-			exit(0);
-		}
-
-		if (command[0] == '\0')
-		{
-			continue;
-		}
-
-		token = strtok(command, " \n");
-		av[0] = malloc(sizeof(char *) * 1024);
-		while (token != NULL)
-		{
-			av[i++] = token;
-			token = strtok(NULL, " \n");
-		}
-		av[i] = NULL;
-
-		pid = fork();
-		if (pid < 0)
-		{
-			fprintf(stderr, "%s\n", argv[0]);
-		}
-		else if (pid == 0)
-		{
-			if (execve(av[0], av, NULL) == -1)
-			{
-				fprintf(stderr, "%s: 1: %s: not found\n ", argv[0], av[0]);
-				exit(1);
-			}
-		}
-		else
-		{
-			wait(&status);
-		}
+			command = readCommand();
+			array = strTokens(command);
+			check_exit(command);
+			executeCommand(array);
 	}
 	free(command);
 	return (0);
