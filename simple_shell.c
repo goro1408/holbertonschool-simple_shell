9ac56268aa3a4c4a6b20ca859089652b6d418519
Gabriel <gabyrivm06@gmail.com>
2024-08-22 16:39:04 -0400

    adding the new code of the main function and the new executable

--- simple_shell.c
+++ simple_shell.c
@@ -8,40 +8,13 @@
  */
 int main(int argc, char **argv)
 {
-    char *command = NULL;
-    char *path;
-    char **array;
-	size_t n = 0;
-	ssize_t nread;
-
-
-	(void)argc;
+    (void)argc;
+    (void)argv;
 
     while (1)
     {
-        prompt();
-		
-		nread = getline(&command, &n, stdin);
-		command[_strcspn(command, "\n")] = '\0';
-		if (nread == -1)
-		{
-			exit(0);
-		}
+        handle_input();
+    }
 
-		if (strcmp(command, "exit") == 0)
-		{
-			free(command);
-			exit(0);
-		}
-		
-		array = strTokens(command);
-        path = get_file_path(array[0]);
-		check_exit(command);
-		executeCommand(array, argv);
-	}
-
-	free(path);
-	free(command);
-	return (0);
+    return (0);
 }
-
